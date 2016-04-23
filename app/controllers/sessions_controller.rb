0b7972a94d949c97a73bbc9399e5e1cdeb7cc893
Rebecca Kern <rkern@andrew.cmu.edu>
2016-04-22 23:36:16 -0400

    login isn't working properly but imma try to fix up some views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@
         redirect_to home_path, notice: "Logged in!"
       else
         flash.now.alert = "Email or password is invalid"
-        render "new"
+        render "new", notice: "#{user} not logged in"
       end
     end
 
