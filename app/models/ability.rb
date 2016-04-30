d7a411eb6a005609dcb1a065c5e3584ae3e74671
Rebecca Kern <rkern@andrew.cmu.edu>
2016-04-30 00:25:00 -0400

    toggle for shifts works appropriate with ability file. start_shift does not. alas we travel on

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,12 +8,15 @@ class Ability
         can :manage, :all
 
     elsif user.role? :manager
-        can :manage, :all
         can :read, Store
         can :read, Flavor
         can :read, Job
 
-      can :index, Employee
+      can :index, Employee do |this_employee|
+        mgr_store = user.employee.current_assignment.store
+        current_employees = mgr_store.assignments.current.map{|a| a.employee}
+        current_employees.include?(this_employee)
+      end 
 
       can :show, Employee do |this_employee|
         mgr_store = user.employee.current_assignment.store
@@ -60,6 +63,20 @@ class Ability
         store_shifts.include?(this_shift)
       end
 
+      can :start_shift, Shift do |this_shift|
+        mgr_store = user.employee.current_assignment.store
+        store_assignments = Assignment.for_store(mgr_store).map{|a| a}#mgr_store.assignments.current.map{|a| a}
+        store_shifts = store_assignments.map{|s| s.shifts}
+        store_shifts.include?(this_shift)
+      end
+
+      can :end_shift, Shift do |this_shift|
+        mgr_store = user.employee.current_assignment.store
+        store_assignments = mgr_store.assignments.current.map{|a| a}
+        store_shifts = store_assignments.map{|s| s.shifts}
+        store_shifts.include?(this_shift)
+      end
+
       can :update, ShiftJob do |this_sj|
         mgr_store = user.employee.current_assignment.store
         store_assignments = mgr_store.assignments.current.map{|a| a}
@@ -76,20 +93,13 @@ class Ability
         store_shift_jobs.include? this_sj
       end
 
-       can :update, StoreFlavor do |this_sf|
-        mgr_store = user.employee.current_assignment.store
-        store_store_flavors = mgr_store.store_flavors.map{|a| a}
-        store_store_flavors.include? this_sf
-      end
-
-      can :destroy, StoreFlavor do |this_sf|
+      can :toggle, StoreFlavor do |this_sf|
         mgr_store = user.employee.current_assignment.store
         store_store_flavors = mgr_store.store_flavors.map{|a| a}
         store_store_flavors.include? this_sf
       end
 
     elsif user.role? :employee
-        can :manage, :all
       can :read, Store
       can :read, Job 
       can :read, Flavor
